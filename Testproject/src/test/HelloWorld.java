4a187d940dd188f7f4966577f9cbfb176f247ed4
Linh To <to@in.tum.de>
2013-10-20 15:40:45 +0200

    test

--- Testproject/src/test/HelloWorld.java
+++ Testproject/src/test/HelloWorld.java
@@ -3,7 +3,7 @@ package test;
 public class HelloWorld {
 
 	public static void main(String[] args){
-		System.out.println("Hello World!");
+		System.out.println("Hello World! Hello World.");
 	}
 	
 }
