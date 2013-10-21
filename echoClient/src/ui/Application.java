d60cac6ef91845397280cabb8b1ae9719e157b7b
Daniel Straub <stradan@hdhc.de>
2013-10-21 19:00:08 +0200

    Set up project structure for milestone 1 Added log4j and set up logging configuration Removed logs directory from git

---
+++ echoClient/src/ui/Application.java
@@ -0,0 +1,15 @@
+package ui;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Application {
+
+	private static Logger logger = LogManager.getLogger();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		logger.info("Hello World!");
+		logger.trace("Test");
+	}
+}
